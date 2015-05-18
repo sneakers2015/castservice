24f2a756a497ffed3d35af2e9d6775faf9e70840
Changyeon Lee <lisyoen@gmail.com>
2015-05-18 14:59:54 +0900

    Create cast.js

---
+++ cast.js
@@ -0,0 +1,14 @@
+function Broadcast(handler) {
+	this.handler = handler || function (msg) {console.log('Broadcast handler not exists');};
+	
+	var socket = io.connect('http://www.dangsam.com:8080');
+	var that = this;
+	socket.on('toclient',function(data){
+		console.log(data.msg);
+		that.handler(data.msg);		
+	});
+	
+	this.send = function (msg) {
+		socket.emit('fromclient',{msg: msg});
+	}
+}
